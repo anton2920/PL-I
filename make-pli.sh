273801c396aac7909c92482107bb510eac26fb1a
anton2920 <anton2920@gmail.com>
2026-06-11 12:43:17 -0500

    experiments: oop: added 'Square' 'class' into example

--- make-pli.sh
+++ make-pli.sh
@@ -3,16 +3,18 @@
 PWD=`pwd`
 PROJECT=`basename $PWD`
 
+PLIOPTS="-qnocompile(e) -qnot=! -qmacro -qmdeck -qmargins(1,200)"
+
 TARGET=$1
 
 case $TARGET in
 '')
-	pli -o $PROJECT $PROJECT.pli -qtest -qnocompile'(e)' -qmacro -qmdeck -qmargins'(1,200)'
+	pli -o $PROJECT $PROJECT.pli -qtest $PLIOPTS
 	;;
 clean)
 	rm -f $PROJECT *.dek *.lst *.map *.o
 	;;
 release)
-	pli -o $PROJECT $PROJECT.pli -qoptimize'(3)' -qnocompile'(e)' -qmacro -qmargins'(1,200)'
+	pli -o $PROJECT $PROJECT.pli -qoptimize'(3)' $PLIOPTS
 	;;
 esac
